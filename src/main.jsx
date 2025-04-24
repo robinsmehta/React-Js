5c90e00eff6c1cbc65cc5812e685df678a3c1f44
Robins Mehta <robinsmehta7@gmail.com>
2025-04-24 17:10:51 +0545

    Exp:test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+<ul>
+  <li>apple</li>
+  <li>banana</li>
+</ul>)
