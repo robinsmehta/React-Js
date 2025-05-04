a935e664b5b1ff7e4157278cfbbb3088c737ecf8
Robins Mehta <robinsmehta7@gmail.com>
2025-05-04 21:35:18 +0545

    feat: test some features

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<ul>
-  <li>apple</li>
-  <li>banana</li>
-</ul>)
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
